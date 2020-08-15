96d5e26da61a60354e53b3faf2bec27617e15508
Karan Shetty <shettykaran2110@gmail.com>
2020-08-15 21:45:41 +0530

    Added animation to svg band

--- app.js
+++ app.js
@@ -9,14 +9,26 @@ window.addEventListener('keydown', playSound);
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  // console.log(key);
+  const drum = document.querySelector(`g[data-key="${e.keyCode}"]`);
+  console.log(drum);
 
   if (!audio) return;
+  if (!key) return;
+  if (!drum) return;
 
   audio.currentTime = 0;
   audio.play();
 
   key.classList.add('playing');
+
+  animateSVG(drum);
+}
+
+function animateSVG(drum) {
+  drum.classList.add('animate');
+  setTimeout(() => {
+    drum.classList.remove('animate');
+  }, 300);
 }
 
 function removeTransition(e) {
